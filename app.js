3c7e2a4dcc52465355c37651cfb3ea309e86a902
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-17 00:27:15 +0200

    Working on validation.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-// alert("it's working")
-
 const form = document.querySelector(".subscribe-to-newsletter");
 const email = document.querySelector(".subscribe-to-newsletter #email");
-// alert("still works");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("abc");
+  validateInput();
 });
 
-// simple regex: ^(.+)@(.+)$
+function validateInput() {
+  let emailValue = email.value.trim();
+
+  if (!isValidEmail(emailValue)) {
+    alert("Invalid email");
+  } else {
+    alert("email goooood");
+    form.submit();
+  }
+}
+
+function isValidEmail(email) {
+  let simpleRegex = /^(.+)@(.+)$/;
+  let complexRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return String(email).toLowerCase().match(complexRegex);
+}
+
+// simple regex: /^(.+)@(.+)$/
+// complex regex: /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/
