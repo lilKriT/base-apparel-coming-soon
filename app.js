bf4a02497d60aabebe55ed7b6e21d4884e047f64
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-16 23:53:50 +0200

    Working on validation.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("it's working")
+// alert("it's working")
+
+const form = document.querySelector(".subscribe-to-newsletter");
+const email = document.querySelector(".subscribe-to-newsletter #email");
+// alert("still works");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("abc");
+});
+
+// simple regex: ^(.+)@(.+)$
