a8f442cda5715e49149000b45b575e3ed045d866
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-01 23:43:46 +0200

    More validation.

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const form = document.querySelector(".subscribe-to-newsletter");
 const email = document.querySelector(".subscribe-to-newsletter #email");
 
+let simpleEmailRegex = /^(.+)@(.+)$/;
+let complexEmailRegex =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 form.addEventListener("submit", (e) => {
-  // e.preventDefault();
-  validateInput();
+  if (!validateInput()) {
+    e.preventDefault();
+  }
 });
 
 function validateInput() {
   let formValid = true;
   let emailValue = email.value.trim();
 
-  if (!isValidEmail(emailValue)) {
-    // alert("Invalid email");
+  if (!isValidEmail(emailValue, complexEmailRegex)) {
     formValid = false;
   }
 
   if (!formValid) {
-    e.preventDefault();
+    return false;
   }
+
+  return true;
 }
 
-function isValidEmail(email) {
-  let simpleRegex = /^(.+)@(.+)$/;
-  let complexRegex =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  return String(email).toLowerCase().match(complexRegex);
+function isValidEmail(email, regex) {
+  return String(email).toLowerCase().match(regex);
 }
-
-// simple regex: /^(.+)@(.+)$/
-// complex regex: /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/
